60c019545e28c8021088cca99d20fea517c519c8
david <dongkpak@gmail.com>
2022-12-30 16:42:21 -0500

    4

--- kid01/settings.py
+++ kid01/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'kid01.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ubuntu',
+        'NAME': 'postgres',
         'User' : 'postgres',
         'PASSWORD':'1019', ## 추가된 부분 
         'HOST':'13.58.160.229' ## 추가된 부분
