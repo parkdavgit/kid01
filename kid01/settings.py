fb3ff35a5688072c7b2ba4aca814f5da8f4a27f5
david <dongkpak@gmail.com>
2022-12-30 20:20:35 -0500

    5

--- kid01/settings.py
+++ kid01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hib=j35@zp!g#)jzpf4(+&zki_u+@)v)pbh#xcvix&ue^^qvwd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-13-58-160-229.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-3-144-200-99.us-east-2.compute.amazonaws.com']
 
 # Application definition
 
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'kid01.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'User' : 'postgres',
-        'PASSWORD':'1019', ## 추가된 부분 
-        'HOST':'13.58.160.229' ## 추가된 부분
-        
+        'NAME': 'usersDB',
+        'User' : 'sammy',
+        'PASSWORD':'pa$$word', ## 추가된 부분 
+        'HOST':'3.144.200.99', ## 추가된 부분
+        'PORT': '5432',
     }
 }
 
