03bc9d756eae0509c0e9613cda5e53d4494818e7
david <dongkpak@gmail.com>
2023-01-02 15:16:23 -0500

    3

--- kiditem/models.py
+++ kiditem/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class Category(models.Model):
+    sort = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.sort)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category')
+    #image = ImageField(upload_to='')
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=0)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add = True)
+    hit = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.pub_date)
