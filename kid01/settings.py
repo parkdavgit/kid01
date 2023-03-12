b76916f59664233178554063cf424bfe053d3e12
david <dongkpak@gmail.com>
2023-03-11 21:31:49 -0500

    a

--- kid01/settings.py
+++ kid01/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'kiditem',
     'sorl.thumbnail',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
--- kiditem/models.py
+++ kiditem/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User, AbstractUser
 from sorl.thumbnail import ImageField
+from django_countries.fields import CountryField
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
 
 
 
@@ -74,3 +80,22 @@ class Order(models.Model):
 
     def __str__(self):
        return '{} by {}'.format(self.products.name, self.user)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+    
