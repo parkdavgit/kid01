a79a245fc2a21100a3606a964bb4695a771920d7
david <dongkpak@gmail.com>
2023-04-05 18:29:14 -0400

    a

--- kiditem/views.py
+++ kiditem/views.py
@@ -97,10 +97,10 @@ def checkout(request, pk):#user.pk =1 or 16
     categories = Category.objects.all()
 
     #initial = {'street_address': address.street_address, 'apartment_address': address.apartment_address, 'country': address.country,'zip': address.zip,'address_type': address.address_type}#
-    initial1 = {'street_address': Address.street_address, 'apartment_address': Address.apartment_address, 'zip': Address.zip,'address_type': Address.address_type}       
+    #initial1 = {'street_address': Address.street_address, 'apartment_address': Address.apartment_address, 'zip': Address.zip,'address_type': Address.address_type}       
     #initial1 = {'street_address': 'a', 'apartment_address': address.apartment_address, 'zip': address.zip,'address_type': address.address_type}
     #form = AddressForm(request.POST, initial=initial)
-    form = AddressForm(request.POST,initial=initial1)
+    form = AddressForm(request.POST)
     if form.is_valid():
         address = form.save(commit=False)
         address.user = request.user
