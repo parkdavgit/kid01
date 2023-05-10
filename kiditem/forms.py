3ce3cf83abf52b0ca743eb249b12b333e0977659
david <dongkpak@gmail.com>
2023-05-10 17:24:14 -0400

    a

--- kiditem/forms.py
+++ kiditem/forms.py
@@ -19,12 +19,5 @@ class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         fields = ['street_address', 'apartment_address', 'zip']
-        widgets = {
-            'street_address': forms.TextInput(attrs={'readonly': 'readonly'}),
-            'apartment_address': forms.TextInput(),
-            #'country': forms.CountryField(multiple=False),
-            #'country': forms.CountryField(multiple=False),
-            'zip': forms.TextInput(),
-            #'address_type': forms.TextInput(),
-        }
+        
 
