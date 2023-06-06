6a0c60d1645ed9f9fae8be8d0807c11296470862
david <dongkpak@gmail.com>
2023-06-05 23:08:33 -0400

    a

--- kiditem/views.py
+++ kiditem/views.py
@@ -94,7 +94,7 @@ def cart_or_buy(request, pk):#product.pk를 urls통해 pk로 받음 갈비
 def address(request,pk):#user.pk =1 or 16
     user = request.user
     address = Address.objects.filter(User=user)#
-    if address.User == user:
+    if address.user == user:
         return redirect('Norder_list', user.pk)   
     
     if request.method == 'POST':
