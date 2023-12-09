834655755e40e7b6ad188bd9026560075c0cf457
david <dongkpak@gmail.com>
2023-12-08 22:34:41 -0500

    a

--- kid01/settings.py
+++ kid01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hib=j35@zp!g#)jzpf4(+&zki_u+@)v)pbh#xcvix&ue^^qvwd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-19-62-88.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-18-118-86-55.us-east-2.compute.amazonaws.com']
 
 # Application definition
 
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'ubuntu',
         'User' : 'ubuntu',
         'PASSWORD':'1019', ## 추가된 부분 
-        'HOST':'3.19.62.88', ## 추가된 부분
+        'HOST':'172.31.29.26', ## 추가된 부분
         'PORT': '5432',
     }
 }
