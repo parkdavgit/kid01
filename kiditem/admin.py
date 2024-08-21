ad7d6fbf67c9679e76c6e2cb69f994bdb13317cf
david <dongkpak@gmail.com>
2024-08-20 22:18:38 -0400

    a

--- kiditem/admin.py
+++ kiditem/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product, Post, Order, Address, Appointment
+from .models import Category, Product, Post, Order, Appointment
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Post)
 admin.site.register(Order)
-admin.site.register(Address)
 admin.site.register(Appointment)
